1dd59b7c01b5cd548ccb76d0f85983a30919a014
yadokani389 <yadokani389@gmail.com>
2026-04-13 08:51:51 +0900

    chore: add flake.nix

---
+++ flake.nix
@@ -0,0 +1,85 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    git-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs@{
+      nixpkgs,
+      flake-parts,
+      ...
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
+
+      imports = with inputs; [
+        git-hooks.flakeModule
+        treefmt-nix.flakeModule
+      ];
+
+      perSystem =
+        {
+          config,
+          pkgs,
+          system,
+          ...
+        }:
+        let
+          toolchain = pkgs.rust-bin.stable.latest.default;
+        in
+        {
+          _module.args.pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ inputs.rust-overlay.overlays.default ];
+          };
+
+          devShells.default = pkgs.mkShell {
+            inputsFrom = [ config.pre-commit.devShell ];
+
+            packages = with pkgs; [
+              toolchain
+              cmake
+              pkg-config
+              rustPlatform.bindgenHook
+              llvmPackages.libclang
+              openssl
+              libxkbcommon
+            ];
+          };
+
+          treefmt = {
+            projectRootFile = "flake.nix";
+            programs = {
+              nixfmt.enable = true;
+              rustfmt.enable = true;
+              taplo.enable = true;
+            };
+          };
+
+          pre-commit.settings = {
+            hooks = {
+              ripsecrets.enable = true;
+              treefmt.enable = true;
+              clippy = {
+                enable = true;
+                packageOverrides.cargo = toolchain;
+                packageOverrides.clippy = toolchain;
+              };
+            };
+          };
+        };
+    };
+}
